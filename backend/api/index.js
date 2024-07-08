f0b862e527abf12a9d944396cc71b421ea99573b
AtiqurRahmanAni <56642339+AtiqurRahmanAni@users.noreply.github.com>
2024-07-08 20:23:34 +0600

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,13 +16,15 @@ const app = express();
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(process.env.DATABASE_URL)
-  .then(() => console.log("Connected to database"))
-  .catch((err) => {
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("Connected to database");
+  } catch (err) {
     console.log(`Error connecting to database ${err}`);
     process.exit(1);
-  });
+  }
+};
 
 const io = new Server(httpServer, {
   pingTimeout: 60000,
@@ -55,6 +57,7 @@ app.use(errorHandler);
 initSocket(io);
 
 httpServer.listen(PORT, () => {
+  connectToDatabase();
   console.log(`Server is listening on port ${PORT}`);
 });
 
