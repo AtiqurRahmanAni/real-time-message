9552d72b798b146527638dfbc065e90c996b8c51
AtiqurRahmanAni <56642339+AtiqurRahmanAni@users.noreply.github.com>
2024-07-16 12:43:36 +0600

    image preview dialog added

---
+++ frontend/src/components/ImagePreviewDialog.jsx
@@ -0,0 +1,53 @@
+import {
+  Transition,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+} from "@headlessui/react";
+import { Fragment } from "react";
+
+const ImagePreviewModal = ({ imageUrl, isOpen, setIsOpen }) => {
+  return (
+    <Transition appear show={isOpen}>
+      <Dialog
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={() => setIsOpen(false)}
+      >
+        <TransitionChild
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        </TransitionChild>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <DialogPanel className="w-full max-w-md rounded-xl bg-white p-1 md:max-w-lg">
+                <div className="rounded-xl overflow-hidden">
+                  <img src={imageUrl} alt="preview image" />
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default ImagePreviewModal;
--- frontend/src/components/Inbox.jsx
+++ frontend/src/components/Inbox.jsx
@@ -4,18 +4,20 @@ import { useAuthContext } from "../context/AuthContextProvider.jsx";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axiosInstance from "../utils/axiosInstance.js";
 import TextInput from "./TextInput.jsx";
-import { useEffect, useRef } from "react";
-import ChatItem from "./ChatItem.jsx";
+import { useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
 import SpinnerBlock from "../assets/Spinner.jsx";
+import ImagePreviewModal from "./ImagePreviewDialog.jsx";
 
+let selectedImageUrl = null;
 const Inbox = () => {
   const selectedConversation = conversationStore(
     (state) => state.selectedConversation
   );
   const { logoutActions } = useAuthContext();
   const messagesEndRef = useRef();
-
+  const { user } = useAuthContext();
+  const [isOpen, setIsOpen] = useState(false);
   const queryClient = useQueryClient();
 
   const cachedMessages = queryClient.getQueryData([
@@ -60,27 +62,75 @@ const Inbox = () => {
     });
   }, [messages, cachedMessages]);
 
+  const onImageClick = (imageUrl) => {
+    selectedImageUrl = imageUrl;
+    setIsOpen(true);
+  };
+
   return (
-    <div className="relative flex-1 min-h-[calc(100dvh-4.45rem)] ml-4">
-      {isLoading ? (
-        <div className="h-full flex justify-center items-center">
-          <SpinnerBlock />
+    <>
+      <div className="relative flex-1 min-h-[calc(100dvh-4.45rem)] ml-4">
+        {isLoading ? (
+          <div className="h-full flex justify-center items-center">
+            <SpinnerBlock />
+          </div>
+        ) : (
+          <ul className="space-y-2 mt-4 h-[calc(100dvh-10rem)] overflow-y-scroll pb-2 px-10 scrollbar-custom">
+            {messages?.data?.map((message) => (
+              <li
+                key={message._id}
+                className={`flex ${
+                  message.senderId === user._id
+                    ? "justify-end text-end"
+                    : "justify-start"
+                }`}
+              >
+                <div
+                  className={`rounded-lg lg:max-w-[50%] ${
+                    message.senderId === user._id
+                      ? "bg-blue-500"
+                      : "bg-gray-400"
+                  }`}
+                >
+                  {message.attachments.length > 0 && (
+                    <div className="flex rounded-lg overflow-hidden">
+                      {message.attachments.map((item, idx) => (
+                        <div
+                          key={idx}
+                          className="cursor-pointer"
+                          onClick={() => onImageClick(item.url)}
+                        >
+                          <img
+                            className="w-full h-full"
+                            src={item.url}
+                            alt="attachment"
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                  <p className={`px-2 py-1 text-white`}>{message.content}</p>
+                </div>
+              </li>
+            ))}
+            <div ref={messagesEndRef} />
+          </ul>
+        )}
+        <div className="absolute bottom-0 w-full">
+          <TextInput
+            disabled={messageSendMutation.isPending || isLoading}
+            onSendButtonClick={(formData) =>
+              messageSendMutation.mutate(formData)
+            }
+          />
         </div>
-      ) : (
-        <ul className="space-y-2 mt-4 h-[calc(100dvh-10rem)] overflow-y-scroll pb-2 px-10 scrollbar-custom">
-          {messages?.data?.map((message) => (
-            <ChatItem key={message._id} message={message} />
-          ))}
-          <div ref={messagesEndRef} />
-        </ul>
-      )}
-      <div className="absolute bottom-0 w-full">
-        <TextInput
-          disabled={messageSendMutation.isPending || isLoading}
-          onSendButtonClick={(formData) => messageSendMutation.mutate(formData)}
-        />
       </div>
-    </div>
+      <ImagePreviewModal
+        imageUrl={selectedImageUrl}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+    </>
   );
 };
 
